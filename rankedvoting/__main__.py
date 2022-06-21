a95bb7cca9e999f36726e31d23c30359ec485a73
Matthew Newland <matthewnnewland@gmail.com>
2022-06-21 10:16:28 -0700

    Add PR part (not rankedvoting strictu sensu but cool)

--- rankedvoting/__main__.py
+++ rankedvoting/__main__.py
@@ -1,4 +1,5 @@
 from typer import Typer
+import typer
 
 
 app = Typer(name="rankedvoting")
@@ -7,13 +8,21 @@ app = Typer(name="rankedvoting")
 @app.command()
 def stv(ballot_file: str, seats: int = 1):
     from .stv import main
+
     main(ballot_file, seats)
 
 
 @app.command()
 def condorcet(ballot_file: str, diff: bool = False):
     from .condorcet import main
+
     main(ballot_file, diff)
+
+
+@app.command()
+def proportional(vote_map_file: str, seats: int = typer.Option(..., "--seats", "-s")):
+    from .proportional import main
+    main(vote_map_file, seats)
 
 
 if __name__ == "__main__":
